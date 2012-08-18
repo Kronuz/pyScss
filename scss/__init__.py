cbf7bf9dedf23d345d4f238a71968b26b6225b65
German M. Bravo <german.mb@deipi.com>
2012-08-18 11:33:43 -0500

    Don't interpret url(//foo) as a comment (thanks to eevee)

--- scss/__init__.py
+++ scss/__init__.py
@@ -356,7 +356,7 @@ _expr_glob_re = re.compile(r'''
 ''', re.VERBOSE)
 
 _ml_comment_re = re.compile(r'\/\*(.*?)\*\/', re.DOTALL)
-_sl_comment_re = re.compile(r'(?<!\w{2}:)\/\/.*')
+_sl_comment_re = re.compile(r'(?<!\burl[(])(?<!\w{2}:)\/\/.*')
 _zero_units_re = re.compile(r'\b0(' + '|'.join(map(re.escape, _zero_units)) + r')(?!\w)', re.IGNORECASE)
 _zero_re = re.compile(r'\b0\.(?=\d)')
 
