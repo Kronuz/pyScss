fb59858585ed060a3d8c0e1265cc8ada8ba50198
Eevee <eevee.git@veekun.com>
2013-01-18 11:02:37 -0800

    Remove _operatorOperands; unused.

--- scss/types.py
+++ scss/types.py
@@ -23,16 +23,6 @@ class ParserValue(object):
 
 class Value(object):
     @staticmethod
-    def _operatorOperands(tokenlist):
-        "generator to extract operators and operands in pairs"
-        it = iter(tokenlist)
-        while 1:
-            try:
-                yield (it.next(), it.next())
-            except StopIteration:
-                break
-
-    @staticmethod
     def _merge_type(a, b):
         if a.__class__ == b.__class__:
             return a.__class__
