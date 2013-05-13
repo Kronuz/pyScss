bdf248b6234643fd59d7f2e72450834a9915ecfe
Eevee <eevee.git@veekun.com>
2013-05-13 15:53:04 -0700

    Slightly smarter test file filtering.

--- scss/tests/conftest.py
+++ scss/tests/conftest.py
@@ -6,15 +6,15 @@ import re
 
 FILES_DIR = os.path.join(os.path.dirname(__file__), 'files')
 
-def pytest_generate_tests(metafunc):
-    """Inject the test files as parametrizations.
-
-    Relies on the command-line option `--test-file-filter`, set by the root
-    conftest.py.
+test_file_pairs = None  # edited below
+def pytest_configure(config):
+    """Scan for test files.  Done here because other hooks tend to run once
+    *per test*, and there's no reason to do this work more than once.
     """
+    global test_file_pairs
 
     scss_files = glob.glob(os.path.join(FILES_DIR, '*/*.scss'))
-    test_file_filter = metafunc.config.getoption('test_file_filter')
+    test_file_filter = config.getoption('test_file_filter')
     if test_file_filter:
         file_filters = [
             re.compile(filt)
@@ -31,6 +31,14 @@ def pytest_generate_tests(metafunc):
 
         scss_files = filtered_scss_files
 
-    metafunc.parametrize(
-        'scss_file_pair',
-        [(fn, fn[:-5] + '.css') for fn in scss_files])
+    test_file_pairs = [(fn, fn[:-5] + '.css') for fn in scss_files]
+
+def pytest_generate_tests(metafunc):
+    """Inject the test files as parametrizations.
+
+    Relies on the command-line option `--test-file-filter`, set by the root
+    conftest.py.
+    """
+
+    if 'scss_file_pair' in metafunc.fixturenames:
+        metafunc.parametrize('scss_file_pair', test_file_pairs)
