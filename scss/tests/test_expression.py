04b2af2b9dd985277e14971b5c4b2ad5d32c0d04
German M. Bravo <german.mb@deipi.com>
2013-08-17 17:47:57 -0500

    Test fixed & PEP8

--- scss/tests/test_expression.py
+++ scss/tests/test_expression.py
@@ -5,13 +5,16 @@ from scss.types import Color, List, Null, Number, String
 
 import pytest
 
+
 @pytest.fixture
 def calc():
     return Calculator().evaluate_expression
 
+
 def assert_strict_string_eq(expected, actual):
     assert expected.value == actual.value
     assert expected.quotes == actual.quotes
+
 
 def test_reference_operations():
     """Test the example expressions in the reference document:
@@ -33,7 +36,7 @@ def test_reference_operations():
     ns.set_variable('$width', Number(1000, "px"))
     ns.set_variable('$font-size', Number(12, "px"))
     ns.set_variable('$line-height', Number(30, "px"))
-    assert calc('10px/8px') == String('10px / 8px') # plain CSS; no division
+    assert calc('10px/8px') == String('10px / 8px')   # plain CSS; no division
     assert calc('$width/2') == Number(500, "px")      # uses a variable; does division
     assert calc('(500px/2)') == Number(250, "px")     # uses parens; does division
     assert calc('5px + 8px/2px') == Number(9, "px")   # uses +; does division
@@ -50,7 +53,7 @@ def test_reference_operations():
     assert calc('opacify($translucent-red, 0.3)') == Color.from_rgb(1, 0, 0, 0.8)
     assert calc('transparentize($translucent-red, 0.25)') == Color.from_rgb(1, 0, 0, 0.25)
     assert calc("progid:DXImageTransform.Microsoft.gradient(enabled='false', startColorstr='#{ie-hex-str($green)}', endColorstr='#{ie-hex-str($translucent-red)}')"
-                ) == "progid:DXImageTransform.Microsoft.gradient(enabled='false', startColorstr='#FF00FF00', endColorstr='#80FF0000')"
+                ) == String.unquoted("progid:DXImageTransform.Microsoft.gradient(enabled='false', startColorstr='#FF00FF00', endColorstr='#80FF0000')")
 
     # String operations
     ns.set_variable('$value', Null())
@@ -60,7 +63,6 @@ def test_reference_operations():
     assert calc('3px + 4px auto') == List([Number(7, "px"), String('auto', quotes=None)])
     assert_strict_string_eq(calc('"I ate #{5 + 10} pies!"'), String('I ate 15 pies!', quotes='"'))
     assert_strict_string_eq(calc('"I ate #{$value} pies!"'), String('I ate  pies!', quotes='"'))
-
 
 
 # Operators: arithmetic (+ - * / %), unary (+ -), comparison (== != < > <= >=), boolean
@@ -87,6 +89,7 @@ def test_addition(calc):
     assert calc('#010305 + #050301') == Color.from_hex('#060606')
     assert calc('#ffffff + #ffffff') == Color.from_name('white')
 
+
 def test_subtraction(calc):
     assert calc('123 - 456') == Number(-333)
     assert calc('456 - 123') == Number(333)
@@ -94,9 +97,11 @@ def test_subtraction(calc):
 
     assert calc('#0f0f0f - #050505') == Color.from_hex('#0a0a0a')
 
+
 def test_division(calc):
     assert calc('(5px / 5px)') == Number(1)
     assert calc('(1in / 6pt)') == Number(12)
+
 
 def test_comparison_numeric(calc):
     assert calc('123 < 456')
@@ -115,6 +120,7 @@ def test_comparison_numeric(calc):
     assert not calc('456 <= 123')
     assert not calc('123 != 123')
     assert not calc('123 == 456')
+
 
 def test_comparison_stringerific(calc):
     assert calc('"abc" == "abc"')
@@ -139,6 +145,7 @@ def test_comparison_stringerific(calc):
 
         with pytest.raises(TypeError):
             calc(expression)
+
 
 def test_comparison_null(calc):
     assert calc('null == null')
