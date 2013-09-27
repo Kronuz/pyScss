142dcfa3e1cd1924ee01816ba0c6fa72d53d238d
Eevee (Alex Munroe) <eevee.git@veekun.com>
2013-09-26 18:18:02 -0700

    Quick CLI smoketest.  #123

---
+++ scss/tests/test_cli.py
@@ -0,0 +1,22 @@
+"""Test the command-line tool from the outside."""
+from subprocess import PIPE, Popen
+
+# TODO: this needs way, way, way, way more tests
+
+def test_stdio():
+    proc = Popen(['python', '-m', 'scss.tool', '-C'], stdin=PIPE, stdout=PIPE)
+    out, _ = proc.communicate("""
+        $color: red;
+
+        table {
+            td {
+                color: $color;
+            }
+        }
+    """)
+
+    assert out == """\
+table td {
+  color: red;
+}
+"""
