ff8a2e7122c7039007093540ea52efe2adfe159d
Eevee (Alex Munroe) <eevee.git@veekun.com>
2014-05-02 17:57:54 -0700

    Revert "Fixed xfail directory in test files"
    This reverts commit 20be3789370fa18cbc054190aebb713cbe4c8975.
    
    Conflicts:
    	scss/tests/test_files.py

--- scss/tests/test_files.py
+++ scss/tests/test_files.py
@@ -12,7 +12,6 @@ from __future__ import absolute_import
 
 import os.path
 import logging
-import pytest
 
 import scss
 
@@ -25,8 +24,7 @@ logger.addHandler(console)
 
 def test_pair_programmatic(scss_file_pair):
     scss_fn, css_fn, pytest_trigger = scss_file_pair
-
-    if pytest_trigger is pytest.skip:
+    if pytest_trigger:
         pytest_trigger()
 
     with open(scss_fn) as fh:
@@ -41,19 +39,11 @@ def test_pair_programmatic(scss_file_pair):
     include_dir = os.path.join(directory, 'include')
     scss.config.STATIC_ROOT = os.path.join(directory, 'static')
 
-    try:
-        compiler = scss.Scss(scss_opts=dict(style='expanded'), search_paths=[include_dir, directory])
-        actual = compiler.compile(source)
-    except Exception:
-        if pytest_trigger is pytest.xfail:
-            pytest_trigger()
-        raise
+    compiler = scss.Scss(scss_opts=dict(style='expanded'), search_paths=[include_dir, directory])
+    actual = compiler.compile(source)
 
     # Normalize leading and trailing newlines
     actual = actual.strip('\n')
     expected = expected.strip('\n')
 
-    if pytest_trigger is pytest.xfail:
-        assert expected != actual
-    else:
-        assert expected == actual
+    assert expected == actual
