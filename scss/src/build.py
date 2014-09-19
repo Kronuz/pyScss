124669c4e10f45a5f1a832dd26eca96fd7887dbb
German M. Bravo <german.mb@deipi.com>
2014-09-18 20:22:25 -0500

    Speedups builder fixes speedup building path

--- scss/src/build.py
+++ scss/src/build.py
@@ -4,22 +4,25 @@ from distutils.core import setup, Extension
 from distutils.command.build_ext import build_ext as _build_ext
 import os
 
+
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
+
 
 class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         self.build_temp = './'
-        self.build_lib = '../'
+        self.build_lib = '../grammar/'
+
 
 if len(sys.argv) == 1:
     sys.argv.append('build')
 
 setup(ext_modules=[
     Extension(
-        '_speedups',
+        '_scanner',
         sources=['_speedups.c', 'block_locator.c', 'scanner.c'],
         libraries=['pcre'],
     ),
