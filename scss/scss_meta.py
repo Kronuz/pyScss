9eb920cfea3b309b37747a668870f494a9ac0533
Eevee (Alex Munroe) <eevee.git@veekun.com>
2014-10-15 16:20:17 -0700

    Bump version to 1.3.0.

--- scss/scss_meta.py
+++ scss/scss_meta.py
@@ -46,8 +46,8 @@ from __future__ import unicode_literals
 
 import sys
 
-VERSION_INFO = (1, 3, 0, 'a1')
-DATE_INFO = (2013, 10, 8)  # YEAR, MONTH, DAY
+VERSION_INFO = (1, 3, 0)
+DATE_INFO = (2014, 10, 15)  # YEAR, MONTH, DAY
 VERSION = '.'.join(str(i) for i in VERSION_INFO)
 REVISION = '%04d%02d%02d' % DATE_INFO
 BUILD_INFO = "pyScss v" + VERSION + " (" + REVISION + ")"
