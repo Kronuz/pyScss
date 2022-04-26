0f6bf4c6aebcfd93d3b9e016330d917d3063851d
Dmitry Marakasov <amdmi3@amdmi3.ru>
2022-04-26 15:26:05 +0300

    Use reliable python interpreter path in test_cli

--- scss/tests/test_cli.py
+++ scss/tests/test_cli.py
@@ -1,4 +1,5 @@
 """Test the command-line tool from the outside."""
+import sys
 from subprocess import PIPE, Popen
 
 # TODO: this needs way, way, way, way more tests
@@ -6,7 +7,7 @@ from subprocess import PIPE, Popen
 
 def test_stdio():
     proc = Popen(
-        ['python', '-m', 'scss.tool', '-C'],
+        [sys.executable, '-m', 'scss.tool', '-C'],
         stdin=PIPE,
         stdout=PIPE,
         # this automatically handles encoding/decoding on py3
